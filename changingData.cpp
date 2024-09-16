78f45d8ce2c8de69de7bde41108bed4953342370
Suniel <Suniel.paudel2047@gmail.com>
2024-09-15 19:57:15 -0700

    Assignments.

---
+++ changingData.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ // changed one of the datatype.
+    double x=10;
+    int y=3;
+    double z= x/y;
+    cout <<z;
+    return 0;
+}
---
+++ jobEligibility.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string citizenshipResponse;
+    bool isUSCitizen;
+    char hasDegree;
+    int yearsExperience;
+
+    cout << "Are you a US citizen? (yes/no): ";
+    getline(cin, citizenshipResponse);
+
+    isUSCitizen = (citizenshipResponse == "yes" || citizenshipResponse == "Yes");
+
+    if (isUSCitizen)
+    {
+        cout << "Do you have a bachelor's degree? (y/n): ";
+        cin >> hasDegree;
+
+        cout << "How many years of experience do you have?: ";
+        cin >> yearsExperience;
+
+        bool hasBachelorDegree = (hasDegree == 'y' || hasDegree == 'Y');
+        bool hasRequiredExperience = (yearsExperience >= 2);
+
+        if (hasBachelorDegree || hasRequiredExperience)
+        {
+            cout << "You are eligible for the job!" <<endl;
+        }
+        else
+        {
+            cout << "You are not eligible for the job." <<endl;
+        }
+    }
+    else
+    {
+        cout << "You must be a US citizen to be eligible for the job." <<endl;
+    }
+    return 0;
+}
