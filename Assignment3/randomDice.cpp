62f8608f666d20d2f04f77a127fcf3466624a4f8
Suniel <Suniel.paudel2047@gmail.com>
2024-09-17 13:08:03 -0700

    added gitignore

---
+++ Assignment3/randomDice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    srand(time(0));
+    int minValue = 1;
+    int maxValue = 6;
+
+    int die1 = (rand() % (maxValue - minValue + 1)) + minValue;
+    int die2 = (rand() % (maxValue - minValue + 1)) + minValue;
+
+    cout << "You rolled: " << die1 << " and " << die2 << endl;
+
+    return 0;
+}
---
+++ Assignment5/findSizes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << "The size of short is:  " << sizeof(short) << endl;
+    cout << "The size of double is: " << sizeof(double) << endl;
+    return 0;
+}
