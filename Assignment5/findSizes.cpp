7d839eeb2a4eee8aeb84fe208390d386f86151ff
Suniel <Suniel.paudel2047@gmail.com>
2024-09-17 13:23:56 -0700

    added all asignment files.

--- Assignment5/findSizes.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-
-    cout << "The size of short is:  " << sizeof(short) << endl;
-    cout << "The size of double is: " << sizeof(double) << endl;
-    return 0;
-}
