a629b6693d6c65acee4355ef83d57cf892fdc011
Kailash paudel <132937643+Sunielpaudel@users.noreply.github.com>
2024-09-17 21:30:07 -0700

    Update findSizes.cpp

--- Assignment5/findSizes.cpp
+++ Assignment5/findSizes.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
 
     cout << "The size of short is:  " << sizeof(short) << endl;
+    cout << "Minimum value of shorts: " << numeric_limits<short>::min() << endl;
+    cout << "Maximum value of shorts: " << numeric_limits<short>::max() << endl;
+    
     cout << "The size of double is: " << sizeof(double) << endl;
+    cout << "Minimum value of double: " << numeric_limits<double>::min() << endl;
+    cout << "Maximum value of double: " << numeric_limits<double>::max() << endl;
+
     return 0;
 }
